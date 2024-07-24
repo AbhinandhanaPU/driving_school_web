456a9014d5c6fded4b522edd0024aa86f104698f
rahullr <rahullr1320@gmail.com>
2024-07-24 13:42:09 +0530

    july 23 ,13:42 commit

--- lib/view/users/admin/screens/practice_shedule/practice_shedule_data_list.dart
+++ lib/view/users/admin/screens/practice_shedule/practice_shedule_data_list.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart'; 
 import 'package:new_project_driving/colors/colors.dart';
+import 'package:new_project_driving/controller/practice_shedule_controller/practice_shedule_controller.dart';
 import 'package:new_project_driving/fonts/text_widget.dart';
 import 'package:new_project_driving/model/practice_shedule_model/practice_shedule_model.dart';
+import 'package:new_project_driving/view/users/admin/screens/practice_shedule/practice_shedule_edit.dart';
+import 'package:new_project_driving/view/widget/custom_delete_showdialog/custom_delete_showdialog.dart';
 import 'package:new_project_driving/view/widget/reusable_table_widgets/data_container.dart';
 
 class PracticeSheduleDataList extends StatelessWidget {
+  final PracticeSheduleController practiceshedulecontroller = Get.put(PracticeSheduleController());
+
   final PracticeSheduleModel data;
   final int index;
-  const PracticeSheduleDataList({
+    PracticeSheduleDataList({
     required this.data,
     required this.index,
     super.key,
@@ -85,12 +91,21 @@ class PracticeSheduleDataList extends StatelessWidget {
           Expanded(
             flex: 3,
             child: Center(
-              child: DataContainerWidget(
-                  rowMainAccess: MainAxisAlignment.center,
-                  color: cWhite,
-                  // width: 150,
-                  index: index,
-                  headerTitle: ' Update 🖋️'),
+              child: GestureDetector(
+                 onTap: () {
+                  practiceshedulecontroller.practiceNameController.text = data.practiceName;
+                  practiceshedulecontroller.startTimeController.text =
+                      data.startTime;
+                  practiceshedulecontroller.endTimeController.text = data.endTime; 
+                  editFunctionOfPractice(context, data);
+                },
+                child: DataContainerWidget(
+                    rowMainAccess: MainAxisAlignment.center,
+                    color: cWhite,
+                    // width: 150,
+                    index: index,
+                    headerTitle: ' Update 🖋️'),
+              ),
             ),
           ), //............................. Student Practce Status
           const SizedBox(
@@ -99,12 +114,26 @@ class PracticeSheduleDataList extends StatelessWidget {
           Expanded(
             flex: 3,
             child: Center(
-              child: DataContainerWidget(
-                  rowMainAccess: MainAxisAlignment.center,
-                  color: cWhite,
-                  // width: 150,
-                  index: index,
-                  headerTitle: ' Remove 🗑️'),
+              child: GestureDetector(
+                  onTap: () {
+                  customDeleteShowDialog(
+                    context: context,
+                    onTap: () {
+                      practiceshedulecontroller.deletePractice(
+                        data.practiceId,
+                        context,
+                      );
+                      Navigator.pop(context);
+                    },
+                  );
+                },
+                child: DataContainerWidget(
+                    rowMainAccess: MainAxisAlignment.center,
+                    color: cWhite,
+                    // width: 150,
+                    index: index,
+                    headerTitle: ' Remove 🗑️'),
+              ),
             ),
           ), //............................. Student Sheduled Practce
         ],
