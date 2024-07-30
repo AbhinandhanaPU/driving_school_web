a682ee9a877e6f9d4f0d616f344dc8d87e366c92
AbhinandhanaPU <abhinandhanapu81@gmail.com>
2024-07-30 17:57:54 +0530

    July 30, practice schedule 17:55 commit --abhinandhana

--- lib/view/users/admin/screens/practice_shedule/practice_shedule_edit.dart
+++ lib/view/users/admin/screens/practice_shedule/practice_shedule_edit.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:new_project_driving/constant/constant.validate.dart';
-import 'package:new_project_driving/controller/practice_shedule_controller/practice_shedule_controller.dart';  
+import 'package:new_project_driving/controller/practice_shedule_controller/practice_shedule_controller.dart';
 import 'package:new_project_driving/model/practice_shedule_model/practice_shedule_model.dart';
 import 'package:new_project_driving/view/widget/custom_showdialouge/custom_showdialouge.dart';
 import 'package:new_project_driving/view/widget/text_formfiled_container/textformfiled_blue_container.dart';
 
-editFunctionOfPractice(BuildContext context, PracticeSheduleModel data) { 
-  final PracticeSheduleController practiceshedulecontroller = Get.put(PracticeSheduleController());
+editFunctionOfPractice(BuildContext context, PracticeSheduleModel data) {
+  final PracticeSheduleController practiceshedulecontroller =
+      Get.put(PracticeSheduleController());
 
   customShowDilogBox(
       context: context,
@@ -18,24 +19,31 @@ editFunctionOfPractice(BuildContext context, PracticeSheduleModel data) {
           child: Column(
             children: [
               TextFormFiledHeightnoColor(
-                  validator: checkFieldEmpty,
-                  controller: practiceshedulecontroller.practiceNameController,
-                  hintText: data.practiceName,
-                  title: 'name'),
+                validator: checkFieldEmpty,
+                controller: practiceshedulecontroller.practiceNameController,
+                hintText: data.practiceName,
+                title: 'name',
+              ),
               TextFormFiledHeightnoColor(
-                  onTap: () async {
-                    practiceshedulecontroller.startTimeController.text =
-                        await dateTimePicker(context);
-                  },
-                  validator: checkFieldTimeIsValid,
-                  controller: practiceshedulecontroller.startTimeController,
-                  hintText: data.startTime,
-                  title: 'start time'),
+                onTap: () async {
+                  practiceshedulecontroller.startTimeController.text =
+                      await timePicker(context);
+                },
+                validator: checkFieldTimeIsValid,
+                controller: practiceshedulecontroller.startTimeController,
+                hintText: data.startTime,
+                title: 'start time',
+              ),
               TextFormFiledHeightnoColor(
-                  validator: checkFieldTimeIsValid,
-                  controller: practiceshedulecontroller.endTimeController,
-                  hintText: data.endTime,
-                  title: 'end time'), 
+                onTap: () async {
+                  practiceshedulecontroller.endTimeController.text =
+                      await timePicker(context);
+                },
+                validator: checkFieldTimeIsValid,
+                controller: practiceshedulecontroller.endTimeController,
+                hintText: data.endTime,
+                title: 'end time',
+              ),
             ],
           ),
         ),
