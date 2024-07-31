b29c381996166cd1aa93e586b71dc6403f62d1ec
AbhinandhanaPU <abhinandhanapu81@gmail.com>
2024-07-31 17:39:16 +0530

    July 31,course std delete issue solved 17:39

--- lib/controller/admin_section/student_controller/student_controller.dart
+++ lib/controller/admin_section/student_controller/student_controller.dart
@@ -2,11 +2,14 @@ import 'dart:developer';
 
 import 'package:get/get.dart';
 import 'package:new_project_driving/constant/const.dart';
+import 'package:new_project_driving/controller/course_controller/course_controller.dart';
 import 'package:new_project_driving/model/student_model/student_model.dart';
 import 'package:new_project_driving/utils/firebase/firebase.dart';
 import 'package:new_project_driving/utils/user_auth/user_credentials.dart';
 
 class StudentController extends GetxController {
+  final CourseController courseController = Get.put(CourseController());
+
   List<StudentModel> studentProfileList = [];
   Rxn<StudentModel> studentModelData = Rxn<StudentModel>();
   RxBool ontapStudent = false.obs;
@@ -43,28 +46,21 @@ class StudentController extends GetxController {
 
   Future<void> deleteStudentsFromCourse(StudentModel studentModel) async {
     try {
-      final docidofcourse = await server
-          .collection('DrivingSchoolCollection')
-          .doc(UserCredentialsController.schoolId)
-          .collection("Courses")
-          .get();
-
-      if (docidofcourse.docs.isNotEmpty) {
-        for (var courseDoc in docidofcourse.docs) {
-          final courseDocid = courseDoc.id;
-
-          // Delete the student from each course
-          await server
-              .collection('DrivingSchoolCollection')
-              .doc(UserCredentialsController.schoolId)
-              .collection("Courses")
-              .doc(courseDocid)
-              .collection('Students')
-              .doc(studentModel.docid)
-              .delete()
-              .then(
-                  (value) => log("Student deleted from course: $courseDocid"));
-        }
+      final data = courseController.courseModelData.value;
+      if (data!.courseId != '') {
+        // Delete the student from each course
+        await server
+            .collection('DrivingSchoolCollection')
+            .doc(UserCredentialsController.schoolId)
+            .collection("Courses")
+            .doc(data.courseId)
+            .collection('Students')
+            .doc(studentModel.docid)
+            .delete()
+            .then((value) {
+          log("Student deleted from course: ${data.courseId}");
+          Get.back();
+        });
       } else {
         log("No courses found");
       }
