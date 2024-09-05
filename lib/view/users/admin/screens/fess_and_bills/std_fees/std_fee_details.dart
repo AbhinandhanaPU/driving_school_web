7f711705128c76956095e1c15e6ac4d8d88ae12e
AbhinandhanaPU <abhinandhanapu81@gmail.com>
2024-09-05 12:42:27 +0530

    Sep 5,fees

--- lib/view/users/admin/screens/fess_and_bills/std_fees/std_fee_details.dart
+++ lib/view/users/admin/screens/fess_and_bills/std_fees/std_fee_details.dart
@@ -9,6 +9,7 @@ import 'package:new_project_driving/utils/firebase/firebase.dart';
 import 'package:new_project_driving/utils/user_auth/user_credentials.dart';
 import 'package:new_project_driving/view/users/admin/screens/fess_and_bills/std_fees/std_fee_datalist.dart';
 import 'package:new_project_driving/view/widget/loading_widget/loading_widget.dart';
+import 'package:new_project_driving/view/widget/progess_button/progress_button.dart';
 import 'package:new_project_driving/view/widget/responsive/responsive.dart';
 import 'package:new_project_driving/view/widget/reusable_table_widgets/category_table_header.dart';
 import 'package:new_project_driving/view/widget/routeSelectedTextContainer/route_NonSelectedContainer.dart';
@@ -68,6 +69,23 @@ class StudentsFeesStatus extends StatelessWidget {
                     ),
                   ),
                   const Spacer(),
+                  Obx(
+                    () => Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: ProgressButtonWidget(
+                        function: () async {
+                          Get.find<NotificationController>().fetchUnpaidUsers(
+                            batchID: feeController.batchId.value,
+                            courseID: courseid,
+                            bodyText: 'fees notification',
+                            titleText: 'Please pay on time',
+                          );
+                        },
+                        buttonstate: notificationController.buttonstate.value,
+                        text: 'Send Notification',
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
@@ -137,85 +155,87 @@ class StudentsFeesStatus extends StatelessWidget {
                         ],
                       ),
                     ),
-                    Obx(
-                      () => Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                              bottom: 8, left: 8, right: 8),
-                          child: StreamBuilder(
-                            stream: feeController.onTapUnpaid.value == true
-                                ? server
-                                    .collection('DrivingSchoolCollection')
-                                    .doc(UserCredentialsController.schoolId)
-                                    .collection('FeesCollection')
-                                    .doc(batchId)
-                                    .collection('Courses')
-                                    .doc(courseid)
-                                    .collection('Students')
-                                    .where('amountPaid', isGreaterThan: 0)
-                                    .snapshots()
-                                : server
-                                    .collection('DrivingSchoolCollection')
-                                    .doc(UserCredentialsController.schoolId)
-                                    .collection('FeesCollection')
-                                    .doc(batchId)
-                                    .collection('Courses')
-                                    .doc(courseid)
-                                    .collection('Students')
-                                    .snapshots(),
-                            builder: (context, snaps) {
-                              if (snaps.hasData &&
-                                  snaps.data!.docs.isNotEmpty) {
-                                return ListView.separated(
-                                  itemBuilder: (context, index) {
-                                    final data = snaps.data!.docs[index].data();
-                                    return StreamBuilder(
-                                        stream: server
-                                            .collection(
-                                                'DrivingSchoolCollection')
-                                            .doc(UserCredentialsController
-                                                .schoolId)
-                                            .collection('Students')
-                                            .doc(data['studentID'])
-                                            .snapshots(),
-                                        builder: (context, snapshot) {
-                                          final modeldata =
-                                              snapshot.data?.data() ?? {};
-                                          final studentModel =
-                                              StudentModel.fromMap(modeldata);
-                                          return StudentFeeDatalist(
-                                            stdData: studentModel,
-                                            index: index,
-                                            feeData: data,
-                                          );
-                                        });
-                                  },
-                                  separatorBuilder: (context, index) {
-                                    return const SizedBox(
-                                      height: 2,
-                                    );
-                                  },
-                                  itemCount: snaps.data!.docs.length,
-                                );
-                              } else if (snaps.data == null) {
-                                return const LoadingWidget();
-                              } else {
-                                return const Center(
-                                  child: Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text(
-                                      "No students Added to fees collection",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w400),
-                                    ),
+                    // Obx(
+                    //   () =>
+                    Expanded(
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.only(bottom: 8, left: 8, right: 8),
+                        child: StreamBuilder(
+                          stream:
+                              // feeController.onTapUnpaid.value == true
+                              //     ? server
+                              //         .collection('DrivingSchoolCollection')
+                              //         .doc(UserCredentialsController.schoolId)
+                              //         .collection('FeesCollection')
+                              //         .doc(batchId)
+                              //         .collection('Courses')
+                              //         .doc(courseid)
+                              //         .collection('Students')
+                              //         .where('paidStatus', isEqualTo: false)
+                              //         .snapshots()
+                              //     :
+                              server
+                                  .collection('DrivingSchoolCollection')
+                                  .doc(UserCredentialsController.schoolId)
+                                  .collection('FeesCollection')
+                                  .doc(batchId)
+                                  .collection('Courses')
+                                  .doc(courseid)
+                                  .collection('Students')
+                                  .orderBy('paidStatus')
+                                  .snapshots(),
+                          builder: (context, snaps) {
+                            if (snaps.hasData && snaps.data!.docs.isNotEmpty) {
+                              return ListView.separated(
+                                itemBuilder: (context, index) {
+                                  final data = snaps.data!.docs[index].data();
+                                  return StreamBuilder(
+                                      stream: server
+                                          .collection('DrivingSchoolCollection')
+                                          .doc(UserCredentialsController
+                                              .schoolId)
+                                          .collection('Students')
+                                          .doc(data['studentID'])
+                                          .snapshots(),
+                                      builder: (context, snapshot) {
+                                        final modeldata =
+                                            snapshot.data?.data() ?? {};
+                                        final studentModel =
+                                            StudentModel.fromMap(modeldata);
+                                        return StudentFeeDatalist(
+                                          stdData: studentModel,
+                                          index: index,
+                                          feeData: data,
+                                        );
+                                      });
+                                },
+                                separatorBuilder: (context, index) {
+                                  return const SizedBox(
+                                    height: 2,
+                                  );
+                                },
+                                itemCount: snaps.data!.docs.length,
+                              );
+                            } else if (snaps.data == null) {
+                              return const LoadingWidget();
+                            } else {
+                              return const Center(
+                                child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "No students Added to fees collection",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w400),
                                   ),
-                                );
-                              }
-                            },
-                          ),
+                                ),
+                              );
+                            }
+                          },
                         ),
                       ),
-                    )
+                    ),
+                    // )
                   ],
                 ),
               ),
